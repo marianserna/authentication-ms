fa545197979b13dccd9460dc2235ed3a325b6384
Marian Serna <marianhalliday@gmail.com>
2017-10-08 20:31:24 -0400

    Creating users_controller + adding attributes to authentication class

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,12 +1,20 @@
 class Authentication
 
-  attr_accessor :params
+  attr_accessor :app_id, :app_secret, :params
 
-  def initialize(params)
+  # when you have several apps connecting to your ms, you need to determine which app is trying to authenticate (mobile app, react app...). Doorkeeper goes to the db and tries to find that app in the oauth_applications table
+  def initialize(app_id, app_secret, params)
+    @app_id = app_id
+    @app_secret = app_secret
     @params = params
   end
 
-  def authenticate
+  # Use case 1: create new user & authenticate automatically
+  def authenticate_new_user
     raise params.inspect
+  end
+
+  # Use case 2: authenticate with password
+  def authenticate_with_apssword
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   use_doorkeeper
 
+  # new happens in the front-end
   resources :users, only: [:create]
 
 end
