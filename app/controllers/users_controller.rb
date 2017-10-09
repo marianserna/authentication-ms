a26b8a5bfd12e61b40d9d1dc63dd889e29b5fdc9
Marian Serna <marianhalliday@gmail.com>
2017-10-08 21:33:24 -0400

    Setting up create action in users_controller to receive user information - strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def create
+    authentication = Authentication.new(
+      ENV.fetch('APP_ID'),
+      ENV.fetch('APP_SECRET'),
+      user_params
+    )
+  end
+
+  private
+
+  def user_params
+    # strong params validate your params 🐷
+    params.require(:user).permit(:name, :email, :password)
   end
 end
