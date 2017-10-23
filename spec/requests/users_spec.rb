f620d756ebd3a8c1beedac48a38f702a2fa4b5b1
Marian Serna <marianhalliday@gmail.com>
2017-10-22 22:08:19 -0400

    Adding tests for user#create

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'users controller' do
+  describe 'create' do
+    it 'creates new user and responds with user + token' do
+      Doorkeeper::Application.create!(
+        name: 'Testing',
+        uid: ENV.fetch('CLIENT_ID'),
+        secret: ENV.fetch('CLIENT_SECRET'),
+        redirect_uri: 'https://localhost:3000'
+      )
+
+      post '/users', {
+        params: {
+          user: {
+            name: "Machete Kill",
+            email: "soymachete@vivamexico.com",
+            password: "deadly",
+            image: fixture_file_upload("#{fixture_path}/machete.jpg")
+          }
+        }
+      }
+
+      expect(response.status).to eq(200)
+
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response["name"]).to eq("Machete Kill")
+      expect(parsed_response["image"]).to be_present
+      expect(User.count).to eq(1)
+    end
+  end
+end
