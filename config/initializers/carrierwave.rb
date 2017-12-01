6752e43f33c62e9ef27622f032825ce132ff38db
Marian Serna <marianhalliday@gmail.com>
2017-11-30 23:22:53 -0500

    Changing bucket name for fog-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       region:                'eu-east-1'
     }
-    config.fog_directory  = 'wanderers_auth'
+    config.fog_directory  = 'wanderersapp'
     config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
   end
 end
